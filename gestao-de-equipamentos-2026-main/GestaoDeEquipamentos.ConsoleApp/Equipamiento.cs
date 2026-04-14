87103d7d349002135761bc27639191672ab805c8
Leo Fzp <Fernandezpatrijuanl93@gmail.com>
2026-04-14 14:28:12 -0300

    first commit

---
+++ gestao-de-equipamentos-2026-main/GestaoDeEquipamentos.ConsoleApp/Equipamiento.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GestaoDeEquipamentos.ConsoleApp;
+
+public class Equipamiento
+{
+    public string id;
+    public string name;
+    public string manufacture;
+    public decimal price;
+    public DateTime manufactureDate;
+}
